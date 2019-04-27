f67c95ad8da1b79ce6afec5d6f9748a5ca809dd2
fa-sousa <fsousa292@gmail.com>
2019-04-27 17:36:23 -0300

    inclusão do jquery e reorganização de pastas do projeto

--- node/project/src/server.js
+++ node/project/src/server.js
@@ -3,9 +3,9 @@ const porta = 3003
 const express = require('express')
 const app = express()
 
-//testando requisições para adaptar ao projeto
-app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.45})//Converter para JSON
+//testando requisições com postman
+app.get('/tarefas', (req, res, next) => {
+    res.send({id: '1', title: 'testando'})//Converter para JSON
 })
 
 app.listen(porta, () => {
