2dd05a2463ff48fde4fdcf333b7460cf6a3c8f8f
fa-sousa <fsousa292@gmail.com>
2019-04-27 19:50:02 -0300

    criação do arquivo banco de dados e teste com o postman

---
+++ node/project/src/bancoDeDados.js
@@ -0,0 +1,22 @@
+const sequence = {
+    _id:1,
+    get id() { return this._id++}
+}
+
+const tarefas = {}
+
+function salvarTarefa(tarefa) {
+    if (!tarefa.id) tarefa.id = sequence.id
+    tarefas[tarefa.id] = tarefa
+    return tarefa
+}
+
+function getTarefa(id) {
+    return tarefas[id] || {}
+}
+
+function getTarefas() {
+    return Object.values(tarefas)
+}
+
+module.exports = {salvarTarefa, getTarefa, getTarefas}
--- node/project/src/server.js
+++ node/project/src/server.js
@@ -2,10 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 //testando requisições com postman
 app.get('/tarefas', (req, res, next) => {
-    res.send({id: '1', title: 'testando'})//Converter para JSON
+    res.send(bancoDeDados.getTarefas())
+})
+
+app.get('/tarefas/:id', (req, res, next) => {
+    res.send(bancoDeDados.getTarefa(req.params.id))
+})
+
+app.post('/tarefas', (req, res, next) => {
+    const tarefa = bancoDeDados.salvarTarefa({
+        id: req.body.name,
+        title: req.body.title,
+        completed: req.body.completed,
+        userId: res.body.userId
+        })
+    res.send(tarefa)
 })
 
 app.listen(porta, () => {
